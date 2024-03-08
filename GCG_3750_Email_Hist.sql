141ae897a3ee013a2014cdb22ab0378944ad0e74
Service Web <sw@gssmail.com>
2024-03-08 15:37:34 -0600

    Commit changes in C:\Websites\Git\emarlow\3750\GCG_3750_Email_Hist.sql

---
+++ GCG_3750_Email_Hist.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "GCG_3750_Email_Hist"(
+ "Contact" VARCHAR(30),
+ "Email" VARCHAR(30),
+ "Subject" VARCHAR(30),
+ "Body" LONGVARCHAR,
+ "Attachment" VARCHAR(50),
+ "Date" VARCHAR(10),
+ "Time" VARCHAR(11));
