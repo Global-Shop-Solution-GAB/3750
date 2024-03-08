1943166040bc5abd069463ca7457506a6cb787e4
Service Web <sw@gssmail.com>
2024-03-08 15:37:34 -0600

    Commit changes in C:\Websites\Git\emarlow\3750\GCG_3750_Email_Type.sql

---
+++ GCG_3750_Email_Type.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_3750_Email_Type"(
+ "Type" VARCHAR(30),
+ "Contact" VARCHAR(30),
+ "Email" VARCHAR(30),
+ "Subject" VARCHAR(30),
+ "Attachment" LONGVARCHAR,
+ "Date" VARCHAR(10),
+ "Time" VARCHAR(11),
+ "Body" LONGVARCHAR);
